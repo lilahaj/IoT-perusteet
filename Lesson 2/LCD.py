83e5701b18380bc1faac9a9d5c35d8ec4ce328d3
lilahaj <fedor.pilotov@gmail.com>
2025-10-06 01:05:51 +0300

    Add LCD display functionality with I2C support

---
+++ Lesson 2/LCD.py
@@ -0,0 +1,10 @@
+import utime
+from machine import Pin, I2C
+from pico_i2c_lcd import I2cLcd
+
+utime.sleep(1)
+
+i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=400000)
+
+lcd = I2cLcd(i2c, 0x27, 4, 20)
+lcd.putstr("Hello student!")
